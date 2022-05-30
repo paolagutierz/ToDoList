b62612d4953b82e9f44a855ae2392ebc9fc24da5
Paola gutierrez <paolagutierzgil@hotmail.com>
2022-05-30 18:41:29 -0500

    se agregaron estados para guardar los TODO

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 
 import Checkbox from "./Checkbox";
-const TaskList = () => {
-	return <div>TaskList!
-        <Checkbox/>
-    </div>;
+
+const TaskList = props => {
+    const { list, setList } = props;
+
+    const onChangeStatus = e => {
+        const { name, checked } = e.target;
+        const updateList = list.map(item => ({
+          ...item,
+          done: item.id === name ? checked : item.done
+        }));
+        setList(updateList);
+      };
+
+      const onClickRemoveItem = e => {
+        const updateList = list.filter(item => !item.done);
+        setList(updateList);
+      };
+
+    const chk = list.map(item => (
+        <Checkbox key={item.id} data={item} onChange={onChangeStatus} />
+      ));
+
+      return (
+        <div className="todo-list">
+          {list.length ? chk : "No tasks"}
+          {list.length ? (
+            <p>
+              <button className="button blue" onClick={onClickRemoveItem}>
+               Eliminar
+              </button>
+            </p>
+          ) : null}
+        </div>
+      );
 };
 
 export default TaskList;
