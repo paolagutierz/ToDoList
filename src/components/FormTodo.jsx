d412c88138434d4e6299b0d23573ca14dc3ae4ef
Paola gutierrez <paolagutierzgil@hotmail.com>
2022-05-30 18:20:13 -0500

    se agrego componente y se corrigio app.js

--- src/components/FormTodo.jsx
+++ src/components/FormTodo.jsx
@@ -1,28 +1,44 @@
 import React, { useState } from "react";
 
-
 const FormTodo = props => {
-    const [description,setDescription]=useState("");
-	return (
-        <form>
-          <div className="todo-list">
-            <div className="file-input">
-              <input
-                type="text"
-                className="text"
-                value={description}
-                onChange={e => setDescription(e.target.value)}
-              />
-              <button
-                className="button pink"
-                disabled={description ? "" : "disabled"}
-              >
-                Add
-              </button>
-            </div>
-          </div>
-        </form>
-      );
+
+  const { handleAddItem } = props; 
+  const [description, setDescription] = useState(""); // (F-1)
+
+  const handleSubmit = e => {
+    e.preventDefault(); 
+ 
+    handleAddItem({
+      done: false,
+      id: (+new Date()).toString(),
+      description
+    });
+    setDescription(""); 
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {/*(D)*/}
+      <div className="todo-list">
+        <div className="file-input">
+          {/*(F-2)*/}
+          <input
+            type="text"
+            className="text"
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+          />
+          {/*(A)*/}
+          <button
+            className="button pink"
+            disabled={description ? "" : "disabled"}
+          >
+            Agregar
+          </button>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default FormTodo;
